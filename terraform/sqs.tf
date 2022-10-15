5ed322b4b23d897f7864c1218b4ada4de8193699
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 01:43:53 -0300

    feature #3 - Add SQS queue

---
+++ terraform/sqs.tf
@@ -0,0 +1,26 @@
+resource "aws_sqs_queue" "update-exchange-rate-queue" {
+  name = "update-exchange-rate-queue"
+  visibility_timeout_seconds  = 30
+}
+
+resource "aws_sqs_queue_policy" "update-exchange-rate-queue-policy" {
+  queue_url = aws_sqs_queue.update-exchange-rate-queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "update-exchange-rate-queue-policy",
+  "Statement": [
+  	{
+			"Sid": "First",
+			"Effect": "Allow",
+			"Principal": {
+				"Service": "events.amazonaws.com"
+			},
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.update_transaction_by_fuelling_partner_queue.arn}",
+    }
+  ]
+}
+POLICY
+}
