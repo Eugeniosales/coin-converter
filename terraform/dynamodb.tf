e5b79a1a075f11489dcddbf968a121c3f2646deb
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 01:35:26 -0300

    Fix resource name

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamo_table" "exchange-rate" {
+resource "aws_dynamodb_table" "exchange-rate" {
   name = "ExchangeRate"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "baseCurrency"
