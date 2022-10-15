ac10cfde7b0c073db4b80267796fbc7ce538a254
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 00:16:24 -0300

    feature #1 - Remove region from terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,8 @@
 terraform {
-  required_version = ">= 0.13"
 
   backend "remote" {
     organization = "Eugeniosales"
-    
-    region = "us-east-1"
-    
+
     workspaces {
       name = "gh-actions-demo"
     }
