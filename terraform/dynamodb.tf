8a4a53fcda89b35a6c47f71762809d8aedecc0b0
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 01:34:10 -0300

    feature #3 - Create dynamo table

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamo_table" "exchange-rate" {
+  name = "ExchangeRate"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "baseCurrency"
+
+  attribute {
+    name = "baseCurrency"
+    type = "S"
+  }
+}
