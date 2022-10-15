a1f3fa38b86e95ec5ac8436da4612e283eb33996
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 01:49:20 -0300

    feature #3 - Add sqs arn in ssm

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,5 +1,12 @@
-resource "aws_ssm_parameter" "ssm-exchange-rate-token" {
-  name  = "/sbf/external/api/exchangerate/token"
+resource "aws_ssm_parameter" "update-exchange-rate-queue-arn-ssm" {
+  name = "/exchangeRate/infra/sqs/updateExchangeRateQueueArn"
+  type = "String"
+  value = aws_sqs_queue.shell_credits_refund_queue.arn
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "exchange-rate-api-token-ssm" {
+  name  = "/exchangeRate/infra/api/exchangeRate/token"
   type  = "String"
-  value = "custom.token"
+  value = ""
 }
