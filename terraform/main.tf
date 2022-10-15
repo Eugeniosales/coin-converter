668e164f095bb30f2a9395892ea24b4694259d16
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 00:37:37 -0300

    feature #1 - Update terraform backend to terraform cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,25 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
 
-  backend "remote" {
+  cloud {
     organization = "Eugeniosales"
 
     workspaces {
       name = "gh-actions-demo"
     }
   }
+}
+
+provider "aws" {
+  region = "us-west-1"
 }
