ecb1728be94cb8c53c00e1a8a9e7814f8415e895
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-16 17:00:07 -0300

    feature #6 - update alb

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -8,3 +8,32 @@ resource "aws_dynamodb_table" "exchange-rate" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "product" {
+  name = "Product"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "category"
+  range_key = "id"
+
+  attribute {
+    name = "category"
+    type = "S"
+  }
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "baseCurrency"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "CategoryBaseCurrencyIndex"
+    hash_key           = "category"
+    range_key          = "baseCurrency"
+    projection_type    = "ALL"
+  }
+}
