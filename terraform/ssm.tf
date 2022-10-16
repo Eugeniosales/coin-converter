c2b196889a66c838b7ccc18868866cd35aa988f8
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-15 23:34:05 -0300

    #4 - add exchange rate internal api url ssm

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -11,3 +11,10 @@ resource "aws_ssm_parameter" "exchange-rate-api-token-ssm" {
   value = "custom"
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "exchange-rate-internal-ssm" {
+  name  = "/exchangerate/infra/alb/arn"
+  type  = "String"
+  value = "http://api.sbf.exchangerate.internal.com"
+  overwrite = true
+}
