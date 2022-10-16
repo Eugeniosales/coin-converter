dea9f6c0d0e4df06bba815b3f70cc001f493b9da
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-16 17:00:36 -0300

    feature #6 - add api gateway event adapter

--- src/4-framework/utility/eventAdapters.ts
+++ src/4-framework/utility/eventAdapters.ts
@@ -1,4 +1,4 @@
-import { ALBEvent } from 'aws-lambda'
+import { ALBEvent, APIGatewayEvent } from 'aws-lambda'
 
 export const albHttpEventNormalizer = (event: ALBEvent) => {
   let payload = {}
@@ -9,6 +9,24 @@ export const albHttpEventNormalizer = (event: ALBEvent) => {
   if (event.body) {
     const body = JSON.parse(event.body)
     payload = { ...payload, ...body }
+  }
+
+  return payload
+}
+
+export const apiGatewayHttpEventNormalizer = (event: APIGatewayEvent) => {
+  let payload = {}
+  if (event.queryStringParameters) {
+    payload = { ...event.queryStringParameters }
+  }
+
+  if (event.body) {
+    const body = JSON.parse(event.body)
+    payload = { ...payload, ...body }
+  }
+
+  if (event.pathParameters) {
+    payload = { ...payload, ...event.pathParameters }
   }
 
   return payload
