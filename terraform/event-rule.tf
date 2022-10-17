11268397a8c4aa720eea48a6d04bfdccdd6c3c21
Eugenio Sales <eugenio.sales@outlook.com>
2022-10-17 00:31:40 -0300

    feature #3 - updte schedule expression from 1 minute to 20 days

--- terraform/event-rule.tf
+++ terraform/event-rule.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_event_rule" "update_exchange_rate_rule" {
   name = "update-exchange-rate-rule"
   description = "update exchange rate rule"
-  schedule_expression = "rate(1 minute)" # Could be replaced to 1 minute or less
+  #schedule_expression = "rate(1 minute)"
+  schedule_expression = "rate(20 days)"
 }
